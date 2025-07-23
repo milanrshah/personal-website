31d2d1ff762908b0b90db1573deb2500a8aea64f
milanrshah <mrs282@cornell.edu>
2025-07-22 20:24:35 -0400

    pages init

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True, use_reloader=True) 
